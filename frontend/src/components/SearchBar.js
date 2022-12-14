f40cf74d3cc65f25216f3e7dcbde2c6b3c765b40
dipesh shakya <dipeshshakya106@gmail.com>
2022-12-14 20:32:52 +1100

    style:Searchbar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SearchBar = () => {
   return (
-    <form className="container relative flex flex-col md:flex-row justify-center items-center m-4 p-10 md:divide-x divide-y">
+    <form className="container relative flex flex-col md:flex-row justify-center items-center m-4 p-10 md:divide-x">
       <input
         type="text"
-        className="p-3 w-full md:w-2/4 focus:outline-none "
+        className="p-3 w-full md:w-1/4 focus:outline-none "
         placeholder="Search for Location..."
       />
       <input
@@ -13,19 +13,19 @@ const SearchBar = () => {
         className="p-3 w-full md:w-1/4 focus:outline-none"
         placeholder="Group Size"
       />
-      <div className="flex flex-row md:w-1/4 focus:outline-none md:divide-x divide-y">
+      <div className="flex flex-row w-full md:w-1/4 focus:outline-none md:divide-x">
         <input
           type="number"
-          className="p-3 w-full focus:outline-none "
+          className="p-3 w-1/2 focus:outline-none "
           placeholder="Min price"
         />
         <input
           type="number"
-          className="p-3 w-full focus:outline-none"
+          className="p-3 w-1/2  focus:outline-none"
           placeholder="Max price"
         />
       </div>
-      <button className="text-white md:w-1/4 bg-sky-500 w-full p-3 text-md">
+      <button className="text-white w-full md:w-1/4 bg-sky-500 p-3 text-md">
         Search
       </button>
     </form>
