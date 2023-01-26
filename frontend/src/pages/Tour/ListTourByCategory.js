51ac51349485184d88ccb0025ee38c62dcb3eda0
dipesh shakya <dipeshshakya106@gmail.com>
2023-01-26 16:21:34 +1100

    refactor:State management with context api

--- frontend/src/pages/Tour/ListTourByCategory.js
+++ frontend/src/pages/Tour/ListTourByCategory.js
@@ -15,25 +15,27 @@ const ListTourByCategory = () => {
   console.log(tour);
 
   return (
-    <div className="card flex flex-col md:flex-row md:justify-between items-center">
-      {tour?.map((item, key) => (
-        <Link to={`/tour/${item.attributes.Slug}`}>
-          <div className="item my-2 md:mx-2" id={++key}>
-            <img
-              src={
-                process.env.REACT_APP_API_URL +
-                item.attributes.Image.data.attributes.formats.thumbnail.url
-              }
-              alt=""
-              className="aspect-square w-full  object-fit rounded-t-lg mb-2 md:mb-8"
-            />
-            <div className=" flex items-center justify-between">
-              <span className="text-5xl"></span>
-              <h2 className="text-xl font-bold ">{item.attributes.Title}</h2>
+    <div className="container mx-auto p-20">
+      <div className="card flex flex-col md:flex-row md:justify-between items-center">
+        {tour?.map((item, key) => (
+          <Link to={`/tour/${item.attributes.Slug}`}>
+            <div className="item my-2 md:mx-2" id={++key}>
+              <img
+                src={
+                  process.env.REACT_APP_API_URL +
+                  item.attributes.Image.data.attributes.formats.thumbnail.url
+                }
+                alt=""
+                className="aspect-square w-full  object-fit rounded-t-lg mb-2 md:mb-8"
+              />
+              <span className="text-xl flex row justify-between ">
+                <h2 className="text-xl font-bold ">{item.attributes.Title}</h2>
+                Rs:{item.attributes.Price}
+              </span>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
